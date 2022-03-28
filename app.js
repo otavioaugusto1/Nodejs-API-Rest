128f065d77f0a925d7597460c2b3edc35fca7d94
Otávio Augusto Gonçalves Nogueira <otavio.a.nogueira@hotmail.com>
2022-03-28 19:56:28 -0300

    uuid, request client

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 //comando para executar após a instalação do nodemon:
 // npm run dev
 const express = require("express");
+const {randomUUID} = require("crypto")
 
 const app = express()
-
+app.use(express.json())
 const products = []
 
 /**
@@ -16,9 +17,21 @@ const products = []
 /**
  * body => Sempre que eu quiser enviar dados para minha aplicação
  * params => parâmetros da url (de rota) ex: /product/123456789
- * Query => /product?id=2345432345432
+ * Query => /product?id=2345432345432&value=122112211221
  */
-app.post('/')
+app.post('/products', (request, response) =>{
+    // Nome e preço => Name e price
+
+    const {name, price} = request.body;
+    const product = {
+        id: randomUUID(),
+        name,
+        price
+    }
+    
+    products.push(product)
+    return response.json(product)
+})
 
 
 app.listen(4002, () => console.log("Servidor está rodando na porta 4002"))
