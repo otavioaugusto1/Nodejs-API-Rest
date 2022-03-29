f1f3df1b1490c427996bb552551e2d15df9b11bc
Otávio Augusto Gonçalves Nogueira <otavio.a.nogueira@hotmail.com>
2022-03-28 21:02:04 -0300

    filesystem

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 // npm run dev
 const express = require("express");
 const {randomUUID} = require("crypto")
+const fs = require("fs");
 
 const app = express()
 app.use(express.json())
@@ -30,11 +31,46 @@ app.post('/products', (request, response) =>{
     }
     
     products.push(product)
+
+    fs.writeFile("Products.json",JSON.stringify(products),(err) => {
+        if(err){
+            console.log(err)
+        }else{
+            console.log("Produto inserido.")
+        }
+    })
     return response.json(product)
 })
 
 app.get("/products",(request,response) =>{
     return response.json(products)
+})
+
+app.get("/products/:id", (request,response) => {
+    const {id} = request.params;
+    const product = products.find(product => product.id === id)
+    return response.json(product)
+})
+
+app.put("/products/:id", (request,response) =>{
+    const {id} = request.params;
+    const {name, price} = request.body;
+
+    const productIndex = products.findIndex(product => product.id === id)
+    products[productIndex] = {
+        ...products[productIndex],
+        name,
+        price
+    }
+
+    return response.json({message: "Produto alterado com sucesso!"})
+})
+
+app.delete("/products/:id", (request, response) =>{
+    const {id} = request.params;
+    const productIndex = products.findIndex(product => product.id === id)
+    products.splice(productIndex,1);
+    return response.json({message: "Produto removido com sucesso!"})
 })
 
 
